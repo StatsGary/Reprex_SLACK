c18ca8ebe95a17e0cc661b883c31090e938f1e2a
Gary Hutson <44023992+StatsGary@users.noreply.github.com>
2021-03-09 15:10:06 +0000

    Update DBILargeData.R

--- DBILargeData.R
+++ DBILargeData.R
@@ -14,14 +14,18 @@ con <- dbConnect(odbc(),
                  Database = db, 
                  Trusted_Connection = "True")
 
-## Create data frame with 80 columns and 20 Million Rows
+## Create data frame with 80 columns and 10 Million Rows
 #dat <- do.call(data.frame, replicate(80, rep(1, 20000000), simplify=FALSE))
+
+
+massive_data <- data.frame(Test=rnorm(50000000, 500, 2))
+massive_data <- cbind(massive_data, rep(massive_data, 10))
 
 
 # Write back to database
 system.time(odbc::dbWriteTable(con, Id(schema = "data", 
                                        table = "bigish_data"), 
-                   dat, append = FALSE, overwrite = TRUE))
+                   massive_data, append = FALSE, overwrite = TRUE))
 # Tweak overwrite to FALSE and append to TRUE for an append query
 
 
